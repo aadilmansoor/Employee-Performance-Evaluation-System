ad8134873f0105c26b39d3351d9d2070878378cc
MOHAMMAD FAWAS CM <favasmcz@gmail.com>
2024-05-14 17:04:18 +0530

    talent trove

--- src/components/ApprovalTable.jsx
+++ src/components/ApprovalTable.jsx
@@ -50,21 +50,7 @@ const ApprovalTable = () => {
     const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
+      
   return (
     <div>
       <TableContainer component={Paper}>
